11a0216a3408241ff89fcbd41f00a3587ec3c319
botino-k <lassmary@bk.ru>
2022-05-23 20:13:32 +0300

    feat: solve 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require("fs/promises");
+const path = require("path");
 
+const myWay = path.join(__dirname, "files");
+const myNewWay = path.join(__dirname, "files-copy");
+
+async function cleanDir() {
+  await fs.rm(myNewWay, { force: true, recursive: true });
+  await fs.mkdir(myNewWay, { recursive: true });
+}
+
+async function copyDir() {
+  await cleanDir();
+ 
+
+  let files = await fs.readdir(myWay, { withFileTypes: true });
+
+  for (let file of files) {
+    await fs.copyFile(
+      path.join(myWay, file.name),
+      path.join(myNewWay, file.name)
+    );
+  }
+}
+copyDir();
