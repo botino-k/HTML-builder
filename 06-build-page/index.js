5ac5dec5f0e5fbf4c659188ba05616e1b3bbf19c
botino-k <lassmary@bk.ru>
2022-05-24 14:17:08 +0300

    feat: solve 06 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,6 +9,8 @@ const templateOriginalP = path.join(__dirname, "template.html");
 const templateNewP =path.join(__dirname, "project-dist", "index.html");
 const stylesOriginalP = path.join(__dirname, "styles");
 const styleNewP = path.join(__dirname, "project-dist", "style.css");
+const assetsP = path.join(__dirname, 'assets');
+const assetsNewP = path.join(__dirname, "project-dist", 'assets');
 
 
  async function cleanAndCreateDir() {
@@ -17,59 +19,69 @@ const styleNewP = path.join(__dirname, "project-dist", "style.css");
 }
 
  async function createHtml() {
+   let componentNameArr = await fsProm.readdir(componentsP, (err) => console.log(err));
+   componentNameArr = componentNameArr.filter((file) => path.extname(file) == ".html");
 
-  let componentNameArr =  await fsProm.readdir(componentsP, err => console.log(err))
-   componentNameArr = componentNameArr.filter(file => path.extname(file) == '.html')
-   
    const rsHTML = fs.createReadStream(templateOriginalP, "utf8");
    const ws = fs.createWriteStream(templateNewP, "utf8");
 
-   rsHTML.on('data', async (chunk) => {
-    //console.log ( chunk)
-   let srt =chunk
-    for (const elem of componentNameArr) {
-      const componentP = path.join(componentsP, elem);
-      const component = await fsProm.readFile(componentP);
-      const baseName = path.basename(elem, '.html');
-     srt = srt.replace(`{{${baseName}}}`, component);
-    }
-    //console.log ( srt)
-   ws.write(srt);
-  
-   })
-  // await fsProm.writeFile(templateNewP, srt, "utf8");
+   rsHTML.on("data", async (chunk) => {
+     let srt = chunk;
+     for (const elem of componentNameArr) {
+       const componentP = path.join(componentsP, elem);
+       const component = await fsProm.readFile(componentP);
+       const baseName = path.basename(elem, ".html");
+       srt = srt.replace(`{{${baseName}}}`, component);
+     }
+     ws.write(srt);
+   });
  }
  
  async function createCSS() {
+   let styleNameArr = await fsProm.readdir(stylesOriginalP, (err) =>console.log(err));
+   styleNameArr = styleNameArr.filter((file) => path.extname(file) == ".css");
 
-  let styleNameArr = await fsProm.readdir(stylesOriginalP, err => console.log(err))
-  styleNameArr = styleNameArr.filter(file => path.extname(file) == '.css')
-  console.log(styleNameArr);
-
-  const ws = fs.createWriteStream(styleNewP, 'utf8');
-    for (const elem of styleNameArr) {
-      const styleP = path.join(stylesOriginalP, elem);
-      const rsCSS = fs.createReadStream(styleP, "utf8");
-      rsCSS.on("data", async (chunk) => {  ws.write(chunk);});
-    }
+   const ws = fs.createWriteStream(styleNewP, "utf8");
+   for (const elem of styleNameArr) {
+     const styleP = path.join(stylesOriginalP, elem);
+     const rsCSS = fs.createReadStream(styleP, "utf8");
+     rsCSS.on("data", async (chunk) => {
+       ws.write(chunk);
+     });
+   }
  }
  
+ async function cleanDir() {
+  await fsProm.rm(assetsNewP, { force: true, recursive: true });
+  await fsProm.mkdir(assetsNewP, { recursive: true });
+}
 
+async function copyAssets(newP, oldP) {
+  await fsProm.mkdir(newP, { recursive: true });
+  const files = await fsProm.readdir(oldP);
 
+console.log(files)
 
+  for (let file of files) {
+    const assetsFile = path.join(oldP, file);
+    const assetsNewFile = path.join(newP, file);
+    const stat = await fsProm.stat(assetsFile);
+    if (stat.isDirectory()) {
+      copyAssets(assetsNewFile, assetsFile);
+    } else {
+      await fsProm.copyFile(assetsFile, assetsNewFile);
+    }
 
+  }
 
+}
 
-
-
-
-
-
- const allTogether = async () => {
+async function allTogether() {
   try {
-    await cleanAndCreateDir();
+   await cleanAndCreateDir();
    await  createHtml();
-   await createCSS()
+   await createCSS();
+   await copyAssets(assetsNewP, assetsP);
   } catch (err) {
     console.error(err);
   }
