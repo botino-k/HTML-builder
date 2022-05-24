1f478243a09130dfaaafe27058c5b2c2a01e8e5d
botino-k <lassmary@bk.ru>
2022-05-24 12:50:46 +0300

    feat: 06 task add CSS bundle

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,24 +7,20 @@ const newFolderP = path.join(__dirname, "project-dist");
 const componentsP = path.join(__dirname, "components");
 const templateOriginalP = path.join(__dirname, "template.html");
 const templateNewP =path.join(__dirname, "project-dist", "index.html");
-
+const stylesOriginalP = path.join(__dirname, "styles");
+const styleNewP = path.join(__dirname, "project-dist", "style.css");
 
 
  async function cleanAndCreateDir() {
   await fsProm.rm(newFolderP, { force: true, recursive: true });
   await fsProm.mkdir(newFolderP, { recursive: true });
-
 }
-
-
 
  async function createHtml() {
 
   let componentNameArr =  await fsProm.readdir(componentsP, err => console.log(err))
    componentNameArr = componentNameArr.filter(file => path.extname(file) == '.html')
    
-   console.log ( componentNameArr)
-
    const rsHTML = fs.createReadStream(templateOriginalP, "utf8");
    const ws = fs.createWriteStream(templateNewP, "utf8");
 
@@ -37,20 +33,43 @@ const templateNewP =path.join(__dirname, "project-dist", "index.html");
       const baseName = path.basename(elem, '.html');
      srt = srt.replace(`{{${baseName}}}`, component);
     }
-    console.log ( srt)
-    ws.write(srt);
+    //console.log ( srt)
+   ws.write(srt);
+  
    })
- 
-   await fsProm.writeFile(templateNewP, srt, "utf8");
-
-
+  // await fsProm.writeFile(templateNewP, srt, "utf8");
  }
  
+ async function createCSS() {
+
+  let styleNameArr = await fsProm.readdir(stylesOriginalP, err => console.log(err))
+  styleNameArr = styleNameArr.filter(file => path.extname(file) == '.css')
+  console.log(styleNameArr);
+
+  const ws = fs.createWriteStream(styleNewP, 'utf8');
+    for (const elem of styleNameArr) {
+      const styleP = path.join(stylesOriginalP, elem);
+      const rsCSS = fs.createReadStream(styleP, "utf8");
+      rsCSS.on("data", async (chunk) => {  ws.write(chunk);});
+    }
+ }
+ 
+
+
+
+
+
+
+
+
+
+
 
  const allTogether = async () => {
   try {
     await cleanAndCreateDir();
    await  createHtml();
+   await createCSS()
   } catch (err) {
     console.error(err);
   }
